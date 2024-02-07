63b82e698d9933b81364ab64b2dc56c1928e86a5
Frank Ma <yurenfrankma@gmail.com>
2024-02-07 11:04:01 -0500

    change to form with validation

--- HW3/script.js
+++ HW3/script.js
@@ -1,20 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const inputElement = document.getElementById("temperature-input");
   const resultElement = document.getElementById("converted-result");
-  const convertElement = document.getElementById("convert-btn");
+  const formElement = document.getElementById("temperature-form");
 
-  function onConvert() {
-    const rawValue = inputElement.value;
+  function onConvert(e) {
+    e.preventDefault();
     const fValue = Number(inputElement.value);
-    if (rawValue === "") {
-      resultElement.innerHTML = "";
-    } else if (fValue < -250 || fValue > 250) {
-      resultElement.innerHTML = "Please input value in range";
-    } else {
-      const converted = ((fValue - 32) * 5) / 9;
-      resultElement.innerHTML = converted;
-    }
+
+    const converted = ((fValue - 32) * 5) / 9;
+    resultElement.innerHTML = converted;
   }
 
-  convertElement.addEventListener("click", onConvert);
+  formElement.addEventListener("submit", onConvert);
 });
