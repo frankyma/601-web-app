b7b0a2cddf683def7659b2a37c54bd7ef54a82bc
Frank Ma <yurenfrankma@gmail.com>
2024-02-21 13:01:44 -0500

    add functional steps

--- HW5/Recipe/src/App.jsx
+++ HW5/Recipe/src/App.jsx
@@ -1,12 +1,41 @@
 import "./App.css";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import {
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [steps, setSteps] = useState([""]);
+
+  const onStepValueChange = useCallback(
+    (e, index) => {
+      const newSteps = steps.map((currStep, currIndex) =>
+        currIndex === index ? e.target.value : currStep
+      );
+      setSteps(newSteps);
+    },
+    [steps]
+  );
+
+  const onDeleteStep = useCallback(
+    (index) => {
+      const newSteps = steps.filter((_, currIndex) => currIndex !== index);
+      setSteps(newSteps);
+    },
+    [steps]
+  );
 
   return (
     <Container
@@ -37,7 +66,41 @@ function App() {
           setDescription(event.target.value);
         }}
       />
-      <Typography variant="h4">Steps</Typography>
+      <Typography variant="h4">
+        Steps
+        <IconButton
+          aria-label="add"
+          onClick={() => {
+            setSteps([...steps, ""]);
+          }}
+        >
+          <AddIcon />
+        </IconButton>
+      </Typography>
+
+      {steps.map((step, index) => (
+        <FormControl variant="outlined" key={index}>
+          <InputLabel htmlFor="outlined-adornment-step">Add step</InputLabel>
+
+          <OutlinedInput
+            id="outlined-adornment-step"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  edge="end"
+                  onClick={() => onDeleteStep(index)}
+                >
+                  {<DeleteIcon />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="step"
+            value={step}
+            onChange={(e) => onStepValueChange(e, index)}
+          />
+        </FormControl>
+      ))}
     </Container>
   );
 }
