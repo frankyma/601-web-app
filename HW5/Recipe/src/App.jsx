93841bf4adb83216421c6c8a0b7de70f49a44278
Frank Ma <yurenfrankma@gmail.com>
2024-02-21 12:28:46 -0500

    form basics, ready for list of steps

--- HW5/Recipe/src/App.jsx
+++ HW5/Recipe/src/App.jsx
@@ -1,7 +1,45 @@
 import "./App.css";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 function App() {
-  return <div>first div</div>;
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h2">Create Recipe</Typography>
+
+      <TextField
+        required
+        id="outlined-required"
+        label="Recipe Name"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <TextField
+        required
+        id="outlined-multiline-static"
+        label="Description"
+        multiline
+        rows={4}
+        value={description}
+        onChange={(event) => {
+          setDescription(event.target.value);
+        }}
+      />
+      <Typography variant="h4">Steps</Typography>
+    </Container>
+  );
 }
 
 export default App;
